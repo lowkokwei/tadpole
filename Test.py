aa32857bf52710701f3bc40a441f27d1d91b59b3
Jim Vella <jim.vella@users.noreply.github.com>
2015-05-23 10:34:11 +0800

    Abstract test class

---
+++ Test.py
@@ -0,0 +1,14 @@
+class Test(object):
+    def __init__(self, name):
+        self.name = name
+
+    def getName(self):
+        return self.name
+
+class Result(object):
+    def __init__(self, passed):
+        self.passed = passed
+
+    def getPassed(self):
+        return self.passed
+
